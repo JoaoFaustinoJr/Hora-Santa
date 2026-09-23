99d4848b3b6d2ca4f7d6da913246e1cbc324b0a3
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-23 15:10:08 -0300

    Atualiza cache v25

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hora-santa-v24';
-const ASSETS=['./','./index.html','./styles.css?v=16','./app.js?v=21','./manifest.webmanifest'];
+const CACHE='hora-santa-v25';
+const ASSETS=['./','./index.html','./styles.css?v=17','./app.js?v=22','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
