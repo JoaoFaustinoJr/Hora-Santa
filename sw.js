98d8cf7ba76185b1948cbf0a9a7b5d93972874d9
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-23 15:11:01 -0300

    Atualiza cache v26 após limpeza

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hora-santa-v25';
+const CACHE='hora-santa-v26';
 const ASSETS=['./','./index.html','./styles.css?v=17','./app.js?v=22','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
