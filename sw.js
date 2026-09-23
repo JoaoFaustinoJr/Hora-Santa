4849c3efb54b6f0888f9fe03a0f3a3805b16cf50
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-23 11:36:05 -0300

    Atualiza cache v11

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hora-santa-v10';
-const ASSETS=['./','./index.html','./styles.css?v=8','./app.js?v=9','./manifest.webmanifest'];
+const CACHE='hora-santa-v11';
+const ASSETS=['./','./index.html','./styles.css?v=9','./app.js?v=10','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
