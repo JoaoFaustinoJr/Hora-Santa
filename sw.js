76564341593f39d154de4628cd5d76fc0499b742
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-23 10:56:46 -0300

    Atualiza cache para Rosário v7

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hora-santa-v6';
-const ASSETS=['./','./index.html','./styles.css?v=5','./app.js?v=5','./manifest.webmanifest'];
+const CACHE='hora-santa-v7';
+const ASSETS=['./','./index.html','./styles.css?v=6','./app.js?v=6','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
