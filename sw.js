c79f15ccf0a91cfbb7cc47de28925ccb1c532914
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-23 11:38:41 -0300

    Renova cache v13

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='hora-santa-v12';
-const ASSETS=['./','./index.html','./styles.css?v=9','./app.js?v=11','./manifest.webmanifest'];
+const CACHE='hora-santa-v13';
+const ASSETS=['./','./index.html','./styles.css?v=9','./app.js?v=12','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
